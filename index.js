aed0b980a8b865f3329942aec1c4005831508c05
Boistean Sergiu Andrei <boistean.sergiu@gmail.com>
2018-05-15 20:22:10 +0300

    All Crud Features

---
+++ index.js
@@ -0,0 +1,60 @@
+const Joi = require('joi')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('<h3>Please use the desire api in the address, like so: /3000/api/name_of_api .</h3>')
+})
+
+mongoose.connect('mongodb://localhost/Cars')
+const db = mongoose.connection
+
+Brand = require('./models/brands.js')
+
+app.get('/all',(req,res)=>{    
+    Brand.getBrands((err, brands) =>{
+        if (err){
+            throw err
+        }
+        res.json(brands)
+    })
+})
+
+app.post('/api/brands',(req,res) => {
+    const brand = req.body
+    Brand.addBrand (brand, (err, brand) =>{
+        if (err){
+            throw err
+        }
+        res.json(brand)
+    })
+})
+
+app.put('/api/modify/:_nume',(req,res) =>{
+    var id = req.params._nume
+    var brand = req.body
+    console.log(id)
+    console.log(brand)
+    Brand.updateBrands(id, brand, {},(err, brand)=>{
+        if (err){
+            throw err
+        }
+        res.json(brand)
+    })
+})
+
+app.delete('/api/delete',(req,res)=>{
+    var brandi = req.body
+    Brand.deleteBrand(brandi,(err, brand) =>{
+        if (err){
+            throw err
+        }
+        res.json(brand)
+    })
+})
+
+app.listen(3000)
+console.log("Waiting on port 3000...")
---
+++ models/brands.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+var  brandSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        requre: true
+    },
+    sold: {
+        type: Number,
+        require: true
+    },
+    returned: {
+        type: Number,
+        require: true
+    }
+},{
+    versionKey: false})
+
+var Brands = module.exports = mongoose.model('Brand', brandSchema)
+
+module.exports.getBrands = (callback, limit) =>{
+    Brands.find(callback).limit(limit)
+}
+
+module.exports.addBrand = (brand, callback) =>{
+    Brands.create(brand, callback)
+}
+
+module.exports.updateBrands = (id, brand,options, callback) =>{
+    const query = { name: id}
+    const update = {
+        name : brand.name,
+        country: brand.country,
+        sold: brand.sold,
+        returned: brand.returned
+    }
+    Brands.findOneAndUpdate(query, update, options, callback)
+}
+
+module.exports.deleteBrand = (brand, callback) =>{
+    const query = {name: brand.name}
+    Brands.remove(query, callback)
+}
